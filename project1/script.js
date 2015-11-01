41c0d8da771c0ab157cb3498293cdd1620be0e3d
IDSFN <nedalkouissi@gmail.com>
2015-11-01 17:52:02 -0500

    Project1 is done! BOOM

--- project1/script.js
+++ project1/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+/* Create Spoiler effect */
+$("p span").hide();
+$("p").append("<button>Click Me</button>");
+$("button").click(function() {
+   $(this).prev().show();
+   $(this).remove();
+});
