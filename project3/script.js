9e5d39b874bac74336af54ac47ec198c2a7cb814
IDSFN <nedalkouissi@gmail.com>
2015-11-09 10:51:05 -0500

    Project 3 is done

--- project3/script.js
+++ project3/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+// Append a select to the header
+"use strict";
+var $select = $("<select></select>");
+$("header").append($select);
+
+// Get the desired infs from anchors inside the header nav
+$("header nav a").each(function() {
+   $a = $(this);
+   $select.append(
+      '<option value="' + $a.attr("href") + '" ' + 
+      ($a.parent().hasClass("selected") ? 'selected':'') + '>' + $a.text() + 
+      '</option>'
+      );
+});
+
+$select.change(function() {
+   window.location = $select.val();
+});
